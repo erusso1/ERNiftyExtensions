e664acf18382810905ead5d5027280036a6c0428
Ephraim Russo <ephraim.s.russo@gmail.com>
2018-03-29 17:48:50 -0700

    Update resized to image scale to 1.0

--- ERNiftyExtensions/Classes/UIImage+Nifty.swift
+++ ERNiftyExtensions/Classes/UIImage+Nifty.swift
@@ -10,9 +10,10 @@ import Foundation
 
 extension UIImage {
     
-    public func resizedTo(_ size: CGSize) -> UIImage? {
+    /// Returns a resized copy of the receiver according to the given `size`. By default, the `scale` is set to 1.0, meaning the dimensions of `size` will be in abolute pixels.
+    public func resizedTo(_ size: CGSize, scale: CGFloat = 1) -> UIImage? {
     
-        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.main.scale)
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
         self.draw(in: CGRect(origin: .zero, size: size))
         let output = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
