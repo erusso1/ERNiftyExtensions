f522f0399a5d3f4fee7987584de4c13fbeabc042
Ephraim Russo <ephraim.s.russo@gmail.com>
2018-06-09 03:39:11 -0700

    Update pod spec.

--- Example/ERNiftyExtensions/ViewController.swift
+++ Example/ERNiftyExtensions/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let view = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        
+        view.backgroundColor = .blue
+        view.cornerRadius = 12
+        
+        self.view.addSubview(view)
     }
 
     override func didReceiveMemoryWarning() {
