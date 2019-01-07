792deebdfbc562d1043177d0ae0122e8a1694481
Ephraim Russo <ephraim.s.russo@gmail.com>
2019-01-07 14:55:27 -0800

    Bump to 3.5.6 - Add UICopyableLabel

---
+++ ERNiftyExtensions/Classes/UIKit/UICopyableLabel.swift
@@ -0,0 +1,61 @@
+//
+//  ERCopyableLabel.swift
+//  SocialBrowser
+//
+//  Created by Ephraim Russo on 10/7/15.
+//  Copyright © 2015 iubble Incorportated. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UICopyableLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        
+        isUserInteractionEnabled = true
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(showMenu(_:)))
+        
+        self.addGestureRecognizer(longPress)
+    }
+    
+    @objc func showMenu(_ sender: AnyObject?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.shared
+        if !menu.isMenuVisible {
+            menu.setTargetRect(bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    override func copy(_ sender: Any?) {
+        
+        let board = UIPasteboard.general
+        board.string = text
+        let menu = UIMenuController.shared
+        menu.setMenuVisible(false, animated: true)
+    }
+
+    override var canBecomeFirstResponder: Bool { return true }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        
+        if action == #selector(copy(_:)) {
+            
+            return true
+        }
+        
+        return false
+    }
+}
